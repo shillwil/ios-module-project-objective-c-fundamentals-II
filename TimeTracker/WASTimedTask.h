01264d9e996a8fd09e50eff418fc8a4485485c69
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-24 16:56:45 -0600

    Delete all organization folders and create TimedTask file with necessary properties

---
+++ TimeTracker/WASTimedTask.h
@@ -0,0 +1,23 @@
+//
+//  WASTimedTask.h
+//  TimeTracker
+//
+//  Created by Alex Shillingford on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WASTimedTask : NSObject
+
+@property (nonatomic, readonly) NSString *client;
+@property (nonatomic, readonly) NSString *workSummary;
+@property (nonatomic, readonly) double hourlyRate;
+@property (nonatomic, readonly) double hoursWorked;
+@property (nonatomic, readonly) double totalAmountDue;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/WASTimedTask.m
@@ -0,0 +1,13 @@
+//
+//  WASTimedTask.m
+//  TimeTracker
+//
+//  Created by Alex Shillingford on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "WASTimedTask.h"
+
+@implementation WASTimedTask
+
+@end
