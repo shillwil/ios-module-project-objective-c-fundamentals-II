923ca6deb928fd765eb86a1d4029143ee5b01692
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-24 20:37:54 -0600

    Create TimeTrackerViewController

---
+++ TimeTracker/WASTimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  WASTimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Alex Shillingford on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WASTimeTrackerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
