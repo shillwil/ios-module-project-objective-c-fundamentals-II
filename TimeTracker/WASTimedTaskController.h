726ecf363e97a1a9fa5763e8d1dab5736ef4920d
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-24 19:58:26 -0600

    Create TimedTaskController

---
+++ TimeTracker/WASTimedTaskController.h
@@ -0,0 +1,17 @@
+//
+//  WASTimedTaskController.h
+//  TimeTracker
+//
+//  Created by Alex Shillingford on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WASTimedTaskController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/WASTimedTaskController.m
@@ -0,0 +1,13 @@
+//
+//  WASTimedTaskController.m
+//  TimeTracker
+//
+//  Created by Alex Shillingford on 5/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "WASTimedTaskController.h"
+
+@implementation WASTimedTaskController
+
+@end
